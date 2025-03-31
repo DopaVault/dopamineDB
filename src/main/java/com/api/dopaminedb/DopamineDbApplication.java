17cc6f2c7335c9b7cd987c0054d228566f8f778a
saltheman <salman.siddiqui@blu-smart.com>
2025-03-31 12:04:53 +0530

    Replaced The Project with Springboot

---
+++ src/main/java/com/api/dopaminedb/DopamineDbApplication.java
@@ -0,0 +1,13 @@
+package com.api.dopaminedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DopamineDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DopamineDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/dopaminedb/DopamineDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.dopaminedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DopamineDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
